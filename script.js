acd8cc6f829c1c12460112c7b4a243bd8cedb645
DeadGnida <fire.vova@mail.ru>
2023-11-20 23:27:17 +0300

    add

---
+++ script.js
@@ -0,0 +1,33 @@
+window.addEventListener('load', async() =>{
+    
+if ('serviceWorker' in navigator){
+    try{
+    const reg = await navigator.serviceWorker.register('/sw.js')
+    console.log('Service worker register success', reg)
+} catch(e) {
+        console.log('Service worker register fail')
+    }
+}
+
+    await loadPosts()
+})
+async function loadPosts() {
+    const res = await fetch('/index.html')
+    const data = await res.json()
+  
+    const container = document.querySelector('#posts')
+    container.innerHTML = data.map(toCard).join('\n')
+  }
+  
+  function toCard(post) {
+    return `
+      <div class="card">
+        <div class="card-title">
+          ${post.title}
+        </div>
+        <div class="card-body">
+          ${post.body}
+        </div>
+      </div>
+    `
+  }
---
+++ sw.js
@@ -0,0 +1,23 @@
+const staticCacheName = 's-app-v1'
+const assetUrls = [
+    'index.html',
+    'script.js',
+    '/css/main.css',
+    'obrat.html',
+    'zapis.html',
+    'card-1.html',
+    'card-2.html',
+    'card-3.html',
+    '/css/card.css',
+    '/css/obrat.css',
+    '/css/zapis.css'
+]
+
+self.addEventListener('istall', Event =>{
+    Event.waitUntil(
+        caches.open(staticCacheName).then(cache => cache.addAll(assetUrls))
+    )
+})
+self.addEventListener('activate', Event =>{
+    console.log('[SW]: activate')
+})
